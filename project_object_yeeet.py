040e1bba669d36ad5cfb700bd6f644078e205e45
Max-Bus <34168923+Max-Bus@users.noreply.github.com>
2017-12-23 19:22:12 -0500

    Update project_object_yeeet.py

--- project_object_yeeet.py
+++ project_object_yeeet.py
@@ -1,1 +1,11 @@
 #all objects
+class characters(object)
+  __init__(self,hp,attck,speed,weapon,potions)
+#includes bosses and bogies
+class room(object)
+  __init__(self,doors,you,loot=0,bogies=0,bos=0)
+#includes all charachters and loot as well as doors
+class loot_crate(object)
+  __init(self,you,bogie=0,boss=0)
+  
+#a box with potions and stuff
