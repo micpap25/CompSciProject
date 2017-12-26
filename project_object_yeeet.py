b05977640a060527f488809998af715f746edd16
yash <yash.choksey@outlook.com>
2017-12-26 14:29:51 -0500

    hiii

--- project_object_yeeet.py
+++ project_object_yeeet.py
@@ -1,11 +1,24 @@
 #all objects
-class characters(object)
-  __init__(self,hp,attck,speed,weapon,potions)
+class characters(object):
+  def __init__(self,hp,attck,speed,weapon,inventory):
+      #we can make inventory a list
+      self.hp=hp
+      self.attck=attck
+      self.speed=speed
+      self.weapon=weapon
+      self.inventory=inventory
 #includes bosses and bogies
-class room(object)
-  __init__(self,doors,you,loot=0,bogies=0,bos=0)
+class room(object):
+  def __init__(self,doors,you,loot=0,bogies=0,bos=0):
+      self.doors=doors
+      self.you=you
+      self.loot=loot
+      self.bogies=bogies
+      self.boss=boss
 #includes all charachters and loot as well as doors
-class loot_crate(object)
-  __init(self,you,bogie=0,boss=0)
+
+class loot_crate(object):
+  def __init(self, loot):
+      self.loot=loot
   
 #a box with potions and stuff
