eed3c7542235c1124c9b318fe4ca80325c1314ae
yash <yash.choksey@outlook.com>
2017-12-26 19:24:51 -0500

    hiii

--- project_object_yeeet.py
+++ project_object_yeeet.py
@@ -24,10 +24,21 @@ class Characters(object):
             luck=random.randint(1,15)
             #now we can make 1-5 empty, 6-12 loot of increasing value(6-8 low) (9-11 middle)(12-godlike), and 13-15......TRAPS!!!(insert evil laugh)
             if luck in [1,2,3,4,5]:
+                self.event+=10
                 #empty chest
             elif luck in [6,7,8,9,10,11,12]:
+                self.event+=10
 
             elif luck in [13,14,15]:
+                if luck==13:
+                    trap=20
+                    self.event += trap
+                elif luck==14:
+                    trap=25
+                    self.event += trap
+                elif luck==15:
+                    trap=0
+                    self.event+=trap
                 #TRAAAAAPPPSSSS!!! YAYAYAYAYYAYAY
 
     def move(self):
@@ -41,11 +52,15 @@ class Room(object):
       self.you=you
       self.loot=loot
       self.bogies=bogies
-      self.boss=boss
+      self.boss=bos
+
 #includes all charachters and loot as well as doors
 
+
+#do we really need to make loot_crate an object? We can just make lists of treasure that get added to the players inventory
 class loot_crate(object):
   def __init(self, loot):
       self.loot=loot
+
   
 #a box with potions and stuff
