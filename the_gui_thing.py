41e84f7f55e5a059278b5b343d8e58a0c81e386e
max_bus <maxbus21@bergen.org>
2018-01-03 13:12:24 -0500

    hi

--- the_gui_thing.py
+++ the_gui_thing.py
@@ -6,9 +6,14 @@ class Room_GUI(tkinter.Frame):
         self.room=Room
         self.characters=Charachters
         self.loot_crate=Loot_crate
+        self.
         self.Room_widgets()
         self.alerts()
     def Room_widgets(self):
-
+        imageSmall = tkinter.PhotoImage(file="images/" + self..small_image)
+        w = tkinter.Label(self,
+                          image=imageSmall,
+                          )
+        w.photo = imageSmall
     def alerts(self):
 
