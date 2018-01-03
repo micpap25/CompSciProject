31c8d4f3791010a36e2d6f7973a2c6d3155fb3d6
max_bus <maxbus21@bergen.org>
2018-01-03 13:05:27 -0500

    hi

--- the_gui_thing.py
+++ the_gui_thing.py
@@ -1,4 +1,14 @@
 from project_object_yeeet import Characters,Room,Loot_crate
+import tkinter
+class Room_GUI(tkinter.Frame):
+    def __init__(self,master,Charachters,Room,Loot_crate):
+        super(Room_GUI,self).__init__(master)
+        self.room=Room
+        self.characters=Charachters
+        self.loot_crate=Loot_crate
+        self.Room_widgets()
+        self.alerts()
+    def Room_widgets(self):
 
-
+    def alerts(self):
 
