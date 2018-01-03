afb44ce4946df3fe069dd70414cdb0842579a166
max_bus <maxbus21@bergen.org>
2018-01-03 13:22:43 -0500

    hi

--- the_gui_thing.py
+++ the_gui_thing.py
@@ -1,19 +1,22 @@
 from project_object_yeeet import Characters,Room,Loot_crate
 import tkinter
 class Room_GUI(tkinter.Frame):
-    def __init__(self,master,Charachters,Room,Loot_crate):
+    def __init__(self,master,Charachters,Room,Loot_crate,small_image):
         super(Room_GUI,self).__init__(master)
         self.room=Room
         self.characters=Charachters
         self.loot_crate=Loot_crate
-        self.
+
+        self.small_image=small_image
         self.Room_widgets()
         self.alerts()
     def Room_widgets(self):
-        imageSmall = tkinter.PhotoImage(file="images/" + self..small_image)
+        imageSmall = tkinter.PhotoImage(file="images/" + "dungeon_floor.jpg")
         w = tkinter.Label(self,
                           image=imageSmall,
                           )
         w.photo = imageSmall
-    def alerts(self):
+root=tkinter.Tk()
+root.title=("HI")
+root.mainloop()
 
