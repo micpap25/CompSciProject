12d6f4288c90d5558c1ff37c80dd3aae4da5417e
Michael Papadopoulos <micpap25@gmail.com>
2018-01-05 13:08:12 -0500

    Hi

--- the_gui_thing.py
+++ the_gui_thing.py
@@ -18,24 +18,28 @@ class Room_GUI(tkinter.Frame):
                           image=imageSmall,
                           )
         w.photo = imageSmall
-        w.grid(row = 3, column = 0, sticky = tkinter.W)
+        w.grid(row = 1, column = 1, columnspan = 2, sticky = tkinter.W)
     def moveButton(self):
-        self.direction = ""
+        self.direction = tkinter.StringVar()
+        self.direction.set(tkinter.NONE)
         #To move, choose a radiobutton then press the movebutton.
         # It passes it off to move in project_objects which changes position
         #We can place the move button above the image, and the radiobuttons in their corresponding places around it.
         self.north = tkinter.Radiobutton(self, text="North", variable=self.direction, value="N")
-        self.north.grid(row = 0, column = 1, sticky = tkinter.W)
+        self.north.grid(row = 0, column = 2, sticky = tkinter.W)
         self.west = tkinter.Radiobutton(self, text="West", variable=self.direction, value="W")
         self.west.grid(row = 1, column = 0, sticky = tkinter.W)
         self.south = tkinter.Radiobutton(self, text="South", variable=self.direction, value="S")
-        self.south.grid( row = 2, column = 1, sticky = tkinter.W)
+        self.south.grid( row = 2, column = 2, sticky = tkinter.W)
         self.east = tkinter.Radiobutton(self, text="East", variable=self.direction, value="E")
-        self.east.grid(row = 1, column = 2, sticky = tkinter.W)
+        self.east.grid(row = 1, column = 3, sticky = tkinter.W)
         self.move_bttn = tkinter.Button(self, text = "Move", command = self.moving)
-        self.move_bttn.grid(row = 1, column = 1, columnspan = 3, sticky = tkinter.W)
+        self.move_bttn.grid(row = 2, column = 3, sticky = tkinter.W)
     def moving(self):
         Characters.moveAround('"fill in whatever the player is"', self.direction)
+    def searchButton(self):
+        self.search_bttn = tkinter.Button(self, text = "Search", command = self.searching)
+    def searching
 
 
 
