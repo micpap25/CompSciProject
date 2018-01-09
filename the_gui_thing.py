e5942ab9c5e991d4a614d3df0db5b3e87b390e2b
Michael Papadopoulos <micpap25@gmail.com>
2018-01-09 10:36:41 -0500

    Hi

--- the_gui_thing.py
+++ the_gui_thing.py
@@ -2,14 +2,15 @@ from project_object_yeeet import Characters,Loot_crate,Room
 from character_make import CharList
 import tkinter
 class Room_GUI(tkinter.Frame):
-    def __init__(self,master, char_list):#,Charachters,Room,Loot_crate):
+    def __init__(self,master):#,Charachters,Room,Loot_crate):
         super(Room_GUI,self).__init__(master)
         #self.room=Room
         #self.characters=Charachters
         #self.loot_crate=Loot_crate
 
         #self.small_image=small_image
-        self.chars = char_list
+        c = CharList("All_DA_Characters.txt")
+        self.chars = c.load_chars()
         self.cho = self.select_character()
         self.player = self.chars[self.cho]
         self.Room_widgets()
