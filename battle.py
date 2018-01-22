20a87f01aa24c9c3eb99bf0046852f83607ff7ff
yash <yash.choksey@outlook.com>
2018-01-22 13:26:13 -0500

    hiii

--- battle.py
+++ battle.py
@@ -1,80 +1,106 @@
-from tkinter import *
-from project_object_yeeet import Characters, Player
-#hio
-class Screen_Battle(Frame):
-    def __init__(self, master, player1, player2, call_on_next):
-        super(Screen_Battle, self).__init__(master)
+import tkinter
+import random
+import winsound
 
-        # Save references to the two player objects
+
+class Screen_Battle(tkinter.Frame):
+    def __init__(self, master, player1, player2, call_on_next, ):
+        super(Screen_Battle, self).__init__(master)
+        # Make a variable so that max HP is set up only once
+        # Save references to the two player object
         self.player1 = player1
         self.player2 = player2
-
         # Store the maximum number of hit points which are needed on the screen display.
-        self.player1_max_hp = player1.hit_points
-        self.player2_max_hp = player2.hit_points
+        self.player1_max_hp = player1.hp  # Need to fix these, max HP is going down to current HP
+        self.player2_max_hp = player2.hp
 
         # Save the method reference to which we return control after this page Exits.
         self.call_on_selected = call_on_next
-
+        self.item()
         self.create_widgets()
         self.grid()
 
     def create_widgets(self):
-        a = Label(text='You')
-        a.grid(row=0, column=0, sticky=W, columnspan=2)
+        tkinter.Label(self, text="you").grid(row=0)
+        imageSmall = tkinter.PhotoImage(file="images2/" + self.player1.pic, master=self)
+        w = tkinter.Label(self, image=imageSmall)
+        w.photo = imageSmall
+        w.grid(row=1, column=0)
+        tkinter.Label(self, text="VS", font=30).grid(row=1, column=1)
+        tkinter.Label(self, text="enemy").grid(row=0, column=2)
+        imageSmall = tkinter.PhotoImage(file="images2/" + self.player2.pic, master=self)
+        w = tkinter.Label(self, image=imageSmall)
+        w.photo = imageSmall
+        w.grid(row=1, column=2)
+        tkinter.Label(self, text="health: " + str(self.player1.hp) + "/" + str(self.player1_max_hp)).grid(row=2,
+                                                                                                          column=0)
+        tkinter.Label(self, text="health: " + str(self.player2.hp) + "/" + str(self.player2_max_hp)).grid(row=2,
+                                                                                                          column=2)
+        self.next = tkinter.Button(self, text="Attack", command=self.attack_clicked, bg="red")
+        self.end = tkinter.Button(self, text="END", command=self.exit_clicked)
+        self.next.grid(row=5, column=4)
 
-        p1image = PhotoImage(file="images/" + self.player1.pic)
-        w = Label(self, image=p1image)
-        w.photo = p1image
-        w.grid(row=1, column=0, sticky=E, columnspan=2)
+    #
+    def item(self):
+        self.menuVar = tkinter.StringVar()
+        self.list = self.player1.inventory
+        if len(self.list) > 0:
+            self.menuVar.set(self.list[0])
+            self.show = tkinter.OptionMenu(self, self.menuVar, *self.list)
+            self.show.grid(row=6, column=0, sticky=tkinter.W)
+            self.useItem = tkinter.Button(self, text="Use item", command=self.using)
+            self.useItem.grid(row=6, column=1, sticky=tkinter.W)
+            self.information = tkinter.Text(self, wrap=tkinter.WORD, width=15, height=5)
+            self.information.grid(row=7, column=0, sticky=tkinter.W)
 
-        self.hp1 = Label(text='HP-' + str(self.player1.hit_points) + '/' + str(self.player1_max_hp))
-        self.hp1.grid(row=2, column=0, sticky=W, columnspan=2)
-        self.victor = Label(text='')
-        self.victor.grid(row=7, column=2, columnspan=2)
-
-        b = Label(text='Computer')
-        b.grid(row=0, column=3, sticky=E, columnspan=2)
-
-
-        self.hp2 = Label(text='HP-' + str(self.player2.hit_points) + "/" + str(self.player2_max_hp))
-        self.hp2.grid(row=2, column=3, sticky=E, columnspan=2)
-
-        p2image = PhotoImage(file = "images/" + self.player2.pic)
-        k = Label(self, image = p2image)
-        k.photo = p1image
-        k.grid(row = 1, column = 3, columnspan = 2)
-
-        self.update = Label(text='')
-        self.update.grid(row=6, column=2, columnspan=2)
-        self.attack = Button(text="Attack", command=self.attack_clicked)
-        self.attack.grid(row=5, column=2, columnspan=2)
-
-        self.exit = Button(text="", command=self.exit_clicked)
+    def using(self):
+        k = self.menuVar.get()
+        info = self.player1.use_item(str(k))
+        self.information.delete(0.0, tkinter.END)
+        self.information.insert(0.0, info)
+        self.list = self.player1.inventory
 
         '''
         This method creates all of the widgets for the battle page.
         '''
-       #There we go
+        #
+        # TO DO
+        #
 
     def attack_clicked(self):
-        self.attack1 = self.player1.s_attack(self.player2)
-        self.attack2 = self.player2.attack(self.player1)
-        '''This is the problem I don't know how to display the damage'''
-        self.update["text"] = self.attack1 + '\n' + self.attack2
+        battle = random.randrange(0, 1)
+        if battle == 0:
+            self.player1.s_attack(self.player2)
+            self.player2.attack(self.player1)
+        if battle == 1:
+            self.player2.attack(self.player1)
+            self.player1.s_attack(self.player2)
+        if self.player1.hp <= 0 or self.player2.hp <= 0:
+            if self.player1.hp <= 0 and self.player2.hp <= 0:
+                tkinter.Label(self, text="It is a tie", font=30).grid(column=1)
+                self.end.grid(column=3)
+                self.next.destroy()
+            elif self.player2.hp <= 0:
+                tkinter.Label(self, text="DEAD", font=30).grid(row=1, column=2)
+                tkinter.Label(self, text="You win", font=30).grid(column=1)
+                p = self.player1.search1()
+                self.item()
+                self.l = tkinter.Label(self, text=p)
+                self.l.grid(columnspan=5)
+                self.end.grid(column=3)
+                self.player1.event -= 5
+                self.next.destroy()
+            elif self.player1.hp <= 0:
+                tkinter.Label(self, text="DEAD", font=30).grid(row=1, column=0)
+                tkinter.Label(self, text="You lose", font=30).grid(column=1)
+                self.end.grid(column=3)
+                self.next.destroy()
 
-        self.hp1["text"] = 'HP:' + str(self.player1.hit_points) + '/' + str(self.player1_max_hp)
-        self.hp2["text"] = 'HP:' + str(self.player2.hit_points) + '/' + str(self.player2_max_hp)
-
-        if self.player1.hit_points <= 0 or self.player2.hit_points <= 0:
-            self.attack.destroy()
-            self.exit["text"] = "Exit"
-            self.exit.grid(row=5, column=2, columnspan=2)
-            if self.player1.hit_points <= 0:
-                self.victor["text"] = str(self.player2.name) + ' is victorious!'
-            else:
-                self.victor["text"] = str(self.player1.name) + ' is victorious!'
-
+        tkinter.Label(self, text="health: " + str(self.player1.hp) + "/" + str(self.player1_max_hp)).grid(row=2,
+                                                                                                          column=0)
+        tkinter.Label(self, text="health: " + str(self.player2.hp) + "/" + str(self.player2_max_hp)).grid(row=2,
+                                                                                                          column=2)
+        self.item()
         ''' This method is called when the user presses the "Attack" button.
 
             This method does the following:
@@ -90,8 +116,5 @@ class Screen_Battle(Frame):
     def exit_clicked(self):
         ''' This method is called when the Exit button is clicked.
             It passes control back to the callback method. '''
+        winsound.PlaySound(None, winsound.SND_ASYNC)
         self.call_on_selected()
-
-
-
-
--- the_gui_thing.py
+++ the_gui_thing.py
@@ -3,16 +3,18 @@ from battle import Screen_Battle
 from project_object_yeeet import Characters,Room,Player,EnemyList
 from character_make import CharList
 import tkinter
+import winsound
 import time
 
 class Room_GUI(tkinter.Frame):
-    def __init__(self,master):#,Charachters,Room,Loot_crate):
+    def __init__(self,master):
         super(Room_GUI,self).__init__(master)
         #self.room=Room
         #self.characters=Charachters
         #self.loot_crate=Loot_crate
         c = CharList("All_DA_Characters.txt")
         self.chars = c.load_chars()
+        self.x = 0
         self.select_character()
         self.grid()
         # self.alerts()
@@ -20,9 +22,10 @@ class Room_GUI(tkinter.Frame):
         c = CharList("All_DA_Characters.txt")
         self.chars = c.load_chars()
         self.character = tkinter.StringVar()
+        winsound.PlaySound(None, winsound.SND_ASYNC)
         for widget in self.winfo_children():
             widget.destroy()
-        self.da_boss=Characters(["DABoss", 150, 50, 65, "Da_boss.png"])
+        self.da_boss=Characters(["DABoss", 150, 50, 65, "Da_boss.gif"])
         self.character.set(None)
         self.hp = tkinter.Label(self, text="Hit Points")
         self.hp.grid(row=0, column=2, sticky=tkinter.W)
@@ -35,7 +38,8 @@ class Room_GUI(tkinter.Frame):
         for c in self.chars:
             cho_button = tkinter.Radiobutton(self, text=c.name, variable=self.character, value=row - 1)
             cho_button.grid(row=row, column=0, sticky=tkinter.W)
-            imageSmall = tkinter.PhotoImage(file="images/" + c.pic)
+            print(c.pic)
+            imageSmall = tkinter.PhotoImage(file="images2/" + c.pic)
             w = tkinter.Label(self, image=imageSmall)
             w.photo = imageSmall
             w.grid(row=row, column=1, sticky=tkinter.W)
@@ -46,9 +50,10 @@ class Room_GUI(tkinter.Frame):
             cre_str = tkinter.Label(self, text=c.attck)
             cre_str.grid(row=row, column=4, sticky=tkinter.W)
             row += 1
+        createchar = tkinter.Button(self,text="custom character",command=self.makechar)
+        createchar.grid()
         nxt_bttn = tkinter.Button(self, text="Next", command=self.move_on)
-        nxt_bttn.grid(row=10, column=4, sticky=tkinter.W)
-
+        nxt_bttn.grid(row=10, column= 4, sticky=tkinter.E)
 
     def move_on(self):
         self.cho = int(self.character.get())
@@ -63,17 +68,51 @@ class Room_GUI(tkinter.Frame):
         self.searchButton()
         self.items()
     def Room_widgets(self):
-        self.imageSmall = tkinter.PhotoImage(file="images/dwarf.jpg")
+        self.imageSmall = tkinter.PhotoImage(file="Images\dungeon.png")
         self.w = tkinter.Label(self,
                           image=self.imageSmall,
                           )
         self.w.photo = self.imageSmall
         self.w.grid(row = 1, column = 1, columnspan = 2, sticky = tkinter.W)
+    def makechar(self):
+        for widgets in self.winfo_children():
+            widgets.destroy()
+        tkinter.Label(self,text="Name").grid()
+        tkinter.Label(self,text=" you have a total of 250 points to put in eh category but no category can exceed 120")
+        tkinter.Label(self,text="Pick a name.").grid()
+        self.n0= tkinter.Entry(self)
+        self.n0.grid()
+        tkinter.Label(self, text="Pick a number of health.").grid()
+        self.n1=tkinter.Entry(self)
+        self.n1.grid()
+        tkinter.Label(self,text="Pick an attack.").grid()
+        self.n2=tkinter.Entry(self)
+        self.n2.grid()
+        tkinter.Label(self, text="Pick a speed.").grid()
+        self.n3=tkinter.Entry(self)
+        self.n3.grid()
+        hi = tkinter.Button(self,text=" confirm", command=self.makechar2)
+        hi.grid()
+    def makechar2(self):
+        if int(self.n1.get()) + int(self.n2.get()) +int(self.n3.get()) >250 or int(self.n1.get())>120 or int(self.n2.get())>120 or int(self.n3.get())>120:
+            self.p = tkinter.Label(self,text="you have exceeded your budget")
+            self.p.grid()
+            root.after(2000,self.p1)
+            self.makechar()
+        else:
+            file=open("All_Da_characters.txt", "a+")
+            file.write("\n" + self.n0.get() +  ", "+ self.n1.get()+", "+self.n2.get()+", " + self.n3.get()+", images.gif")
+            file.close()
+            for widgets in self.winfo_children():
+                widgets.destroy()
+            self.select_character()
+    def p1(self):
+        self.p.destroy()
     def moveButton(self):
         p = random.randrange(0, 3)
+
         self.direction = tkinter.StringVar()
         self.direction.set(tkinter.NONE)
-        self.disclaimer=tkinter.Label(self,text="you will automatically be moved east if you don't enter a field").grid(row=5,column=1,columnspan=5)
         self.north = tkinter.Radiobutton(self, text="North", variable=self.direction, value="N")
 
         if p <= 0:
@@ -93,6 +132,10 @@ class Room_GUI(tkinter.Frame):
 
         self.move_bttn = tkinter.Button(self, text="Move", command=self.moving)
         self.move_bttn.grid(row=2, column=3, sticky=tkinter.W)
+        t = random.randrange(0,7)
+        if t in [1, 2] and self.x > 0:
+            self.merchant()
+        self.x += 1
     def moving(self):
         self.player.event += 5
         self.imageSmall = tkinter.PhotoImage(file="images\dungeon.png")
@@ -103,6 +146,7 @@ class Room_GUI(tkinter.Frame):
         self.south.destroy()
         self.west.destroy()
         self.north.destroy()
+        self.items()
         if self.search_bttn.winfo_exists()==0:
             self.searchButton()
 
@@ -123,6 +167,7 @@ class Room_GUI(tkinter.Frame):
                 widget.destroy()
             self.p = tkinter.Label(self, text="You Lose")
             self.p.grid()
+            winsound.PlaySound('sound\Rick-Astley-Never-Gonna-Give-You-Up.wav', winsound.SND_ASYNC)
             self.replay = tkinter.Button(self, text="replay?", command=self.select_character)
             self.replay.grid()
 
@@ -145,12 +190,12 @@ class Room_GUI(tkinter.Frame):
             self.replay.grid()
         if self.da_boss.hp > 0 and self.player.hp > 0:
             MyDialog(root,self.player, self.da_boss)
-
         if self.player.hp<=0:
             for widget in self.winfo_children():
                 widget.destroy()
             self.p = tkinter.Label(self,text="You Lose")
             self.p.grid()
+            winsound.PlaySound('sound\Rick-Astley-Never-Gonna-Give-You-Up.wav', winsound.SND_ASYNC)
             self.replay=tkinter.Button(self,text="replay?",command=self.select_character)
             self.replay.grid()
     def l_destroy(self):
@@ -176,16 +221,66 @@ class Room_GUI(tkinter.Frame):
         self.show.destroy()
         self.show = tkinter.OptionMenu(self, self.menuVar, *self.list)
         self.show.grid(row=4, column=0, sticky=tkinter.W)
+    def merchant(self):
+        self.inventory = []
+        r = ["Potion: 10 Gold", "Speed Potion: 15 Gold", "Strength Potion: 15 Gold", "Strange potion: 5 Gold"]
+        for i in range(0, 4):
+            random_index = random.randrange(0, len(r))
+            self.inventory.append(r[random_index])
+        self.player.money=10
+        self.widgets1()
+    def widgets1(self):
+        for widgets in self.winfo_children():
+            widgets.destroy()
+        self.menuVar1 = tkinter.StringVar()
+        self.list = self.inventory
+        tkinter.Label(self,text="you have"+str(self.player.money)+"gold coins").grid()
+        if len(self.list) > 0:
+            self.menuVar1.set(self.list[0])
+            self.show = tkinter.OptionMenu(self, self.menuVar1, *self.list)
+            self.show.grid(row=4, column=0, sticky=tkinter.W)
+            self.useItem1 = tkinter.Button(self, text="buy item", command=self.buy)
+            self.useItem1.grid(row=4, column=1, sticky=tkinter.W)
+            self.information1 = tkinter.Text(self, wrap=tkinter.WORD, width=15, height=5)
+            self.information1.grid(row=5, column=0, sticky=tkinter.W)
+        self.exit = tkinter.Button(self,text="Exit",command=self.clear)
+        self.exit.grid()
+    def clear(self):
+        for widgets in self.winfo_children():
+            widgets.destroy()
+        self.moving()
+    def buy(self):
+        self.choice = self.menuVar1.get()
+        if self.choice == "Strange potion: 5 Gold" and self.player.money >= 5:
+            self.player.inventory.append("Strange Potion")
+            self.player.money -= 5
+            w = "you now have a strange potion"
+            self.information1.insert(0.0,w)
+        if self.choice == "Potion: 10 Gold" and self.player.money >= 10:
+            self.player.inventory.append("Potion")
+            self.player.money -= 10
+            w = "you now have a potion"
+            self.information1.insert(0.0, w)
+        if self.choice == "Speed Potion: 15 Gold" and self.player.money >= 15:
+            self.player.speed += 10
+            self.player.money -= 15
+            w = "you now have a speed potion"
+            self.information1.insert(0.0, w)
+        if self.choice == "Strength Potion: 15 Gold" and self.player.money >= 15:
+            self.player.attck += 10
+            self.player.money -= 15
+            w = "you now have a strength potion"
+            self.information1.insert(0.0, w)
 
 class MyDialog:
 
     def __init__(self, parent,player,da_boss):
         self.player=player
         Room(self.player,da_boss)
+
 root=tkinter.Tk()
 root.title("HI")
 app=Room_GUI(root)
 root.mainloop()
 
-#HI
-
+#HI1
